78a45e9c671c13cae419e5080c73068a5268b6e8
haritsahm <haritsahm@gmail.com>
2023-03-25 16:04:30 +0700

    Add fiftyone dataset format parser and test script

---
+++ src/data/components/fiftyone_parser.py
@@ -0,0 +1,53 @@
+import json
+import os
+from typing import Any
+
+import fiftyone
+from torch.utils.data import Dataset
+
+
+class FiftyOneDatasetParser(Dataset):
+    """FiftyoneDataset Format Parser.
+
+    Parse FiftyoneDataset format to torch Dataset.
+    Reference: https://docs.voxel51.com/api/fiftyone.types.html#fiftyone.types.FiftyOneDataset
+
+    Parameters
+    ----------
+    path : str
+        Path to dataset.
+    stage : str, optional
+        Stage to apply augmentation, by default 'train'
+    transform : Any, optional
+        Transformation function, by default None
+
+    Raises
+    ------
+    ValueError
+        If the dataset has 0 samples.
+    """
+
+    def __init__(self, path: str, stage: str = 'train', transform: Any = None):
+        self._dataset = fiftyone.Dataset.from_dir(
+            dataset_dir=path,
+            dataset_type=fiftyone.types.FiftyOneDataset,
+        )
+
+        if len(self._dataset) == 0:
+            raise ValueError('The dataset has 0 samples.')
+
+        with open(os.path.join(path, 'samples.json')) as f:
+            metadata = json.load(f)
+            self._sample_ids = {idx: os.path.join(path, sample['filepath'])
+                                for idx, sample in enumerate(metadata['samples'])}
+
+        self._stage = stage
+        self._transform = transform
+
+    def __len__(self) -> int:
+        """Get number of samples in the dataset."""
+        return len(self._dataset)
+
+    def __getitem__(self, idx: int) -> Any:
+        """Get sample by index."""
+        raise NotImplementedError
---
+++ tests/test_fiftyone_parser.py
@@ -0,0 +1,45 @@
+from typing import Any
+
+import fiftyone
+import pytest
+
+from src.data.components.fiftyone_parser import FiftyOneDatasetParser
+
+
+class QuickstartDataset(FiftyOneDatasetParser):
+    def __getitem__(self, idx: int) -> Any:
+        sample = self._dataset[self._sample_ids[idx]]
+        return sample['filepath']
+
+
+@pytest.fixture(scope='session', autouse=True)
+def quickstart_dataset_path(tmp_path_factory):
+    """Download quickstart dataset."""
+    temp_dir = tmp_path_factory.mktemp('temp_quickstart_dataset')
+    _, dataset_dir = fiftyone.zoo.download_zoo_dataset(
+        'quickstart', dataset_dir=str(temp_dir))
+    return dataset_dir
+
+
+def test_valid_dataset(quickstart_dataset_path):
+    dataset = QuickstartDataset(path=quickstart_dataset_path)
+
+    assert len(dataset) == 200
+    assert isinstance(dataset[0], str)
+
+
+def test_invalid_empty_dataset(quickstart_dataset_path):
+    dataset = fiftyone.Dataset.from_dir(
+        dataset_dir=quickstart_dataset_path, dataset_type=fiftyone.types.FiftyOneDataset)
+    dataset.clear()
+    dataset.save()
+    dataset.export(
+        export_dir=quickstart_dataset_path,
+        dataset_type=fiftyone.types.FiftyOneDataset,
+    )
+    del dataset
+
+    with pytest.raises(Exception) as exc_info:
+        _ = QuickstartDataset(path=quickstart_dataset_path)
+
+    assert str(exc_info.value) == 'The dataset has 0 samples.'

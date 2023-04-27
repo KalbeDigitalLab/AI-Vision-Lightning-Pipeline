8b44949ac6c847950eb06a20db97f4cf51a03d7d
haritsahm <haritsahm@gmail.com>
2023-04-27 10:44:19 +0700

    Refactor fiftyone dataset testings

--- tests/dummy_dataset.py
+++ tests/dummy_dataset.py
@@ -13,60 +13,126 @@ def vindr_mammography_dataset_dir(tmp_path_factory):
     birads_level = [f'BI-RADS {i}' for i in range(6)]
     density_level = [f'DENSITY {i}' for i in ['A', 'B', 'C', 'D']]
 
-    root_dir = tmp_path_factory.mktemp('temp_mammography_vindr_dataset')
+    root_dir = tmp_path_factory.mktemp('temp_mammography_4views_vindr_dataset')
 
     dataset = fiftyone.Dataset()
     num_samples = 40
-    for i in range(num_samples):
-        group = fiftyone.Group()
-        study_id = xxhash.xxh64().hexdigest()
-        samples = []
-        split = random.choice(['train', 'test'])  # nosec B311
-
-        for g_id, side_view in enumerate(['left_cc', 'left_mlo', 'right_cc', 'right_mlo']):
-            # Save to PNG format
-
-            os.makedirs(os.path.join(str(root_dir), 'images', study_id), exist_ok=True)
-            png_file_path = os.path.join(str(root_dir), 'images', study_id, str(g_id)+'.png')
-            image = np.random.randint(0, 256, size=(384, 384))
-            cv2.imwrite(png_file_path, image)  # lossless
-
-            # Build metadata
-            metadata = fiftyone.ImageMetadata.build_for(png_file_path)
-            sample = fiftyone.Sample(
-                filepath=png_file_path,
-                metadata=metadata,
-            )
-
-            tags = []
-            detections = []
-            breast_density = ''
-            breast_birads = ''
-            laterality = ''
-            view_position = ''
-
-            laterality, view_position = side_view.split('_')
+    for split in ['train', 'test']:
+        for i in range(num_samples):
+            group = fiftyone.Group()
+            study_id = xxhash.xxh64().hexdigest()
+            samples = []
             breast_birads = random.choice(birads_level)  # nosec B311
             breast_density = random.choice(density_level)  # nosec B311
-            tags += [split, laterality, view_position]
 
-            # Remove duplicates and add to sample tags
-            tags = set(tags)
-            for tag in tags:
-                sample.tags.append(tag)
+            for g_id, side_view in enumerate(['left_cc', 'left_mlo', 'right_cc', 'right_mlo']):
+                # Save to PNG format
 
-            # Grouped sample
-            sample['view_side'] = group.element(f'{laterality}_{view_position}')
-            sample['laterality'] = laterality
-            sample['view_position'] = view_position
-            sample['breast_birads'] = fiftyone.Classification(label=breast_birads)
-            sample['breast_density'] = fiftyone.Classification(label=breast_density)
-            sample['findings_objects'] = fiftyone.Detections(detections=detections)
-            sample['study_id'] = study_id
+                os.makedirs(os.path.join(str(root_dir), 'images', study_id), exist_ok=True)
+                png_file_path = os.path.join(str(root_dir), 'images', study_id, str(g_id)+'.png')
+                image = np.random.randint(0, 256, size=(384, 384))
+                cv2.imwrite(png_file_path, image)  # lossless
 
-            samples.append(sample)
+                # Build metadata
+                metadata = fiftyone.ImageMetadata.build_for(png_file_path)
+                sample = fiftyone.Sample(
+                    filepath=png_file_path,
+                    metadata=metadata,
+                )
 
-        dataset.add_samples(samples)
+                tags = []
+                detections = []
+                laterality = ''
+                view_position = ''
+
+                laterality, view_position = side_view.split('_')
+                tags += [split, laterality, view_position]
+
+                # Remove duplicates and add to sample tags
+                tags = set(tags)
+                for tag in tags:
+                    sample.tags.append(tag)
+
+                # Grouped sample
+                sample['view_side'] = group.element(f'{laterality}_{view_position}')
+                sample['laterality'] = laterality
+                sample['view_position'] = view_position
+                sample['breast_birads'] = fiftyone.Classification(label=breast_birads)
+                sample['breast_density'] = fiftyone.Classification(label=breast_density)
+                sample['findings_objects'] = fiftyone.Detections(detections=detections)
+                sample['study_id'] = study_id
+
+                samples.append(sample)
+
+            dataset.add_samples(samples)
+
+    output_dir = os.path.join(str(root_dir), 'temp_group_dataset')
+    dataset.export(
+        export_dir=output_dir,
+        dataset_type=fiftyone.types.FiftyOneDataset,
+        export_media=True
+    )
+
+    return output_dir
+
+
+@pytest.fixture(scope='session')
+def vindr_2views_mammography_dataset_dir(tmp_path_factory):
+    birads_level = [f'BI-RADS {i}' for i in range(6)]
+    density_level = [f'DENSITY {i}' for i in ['A', 'B', 'C', 'D']]
+
+    root_dir = tmp_path_factory.mktemp('temp_mammography_2views_vindr_dataset')
+
+    dataset = fiftyone.Dataset()
+    num_samples = 40
+
+    for split in ['train', 'test']:
+        for i in range(num_samples):
+            group = fiftyone.Group()
+            study_id = xxhash.xxh64().hexdigest()
+            samples = []
+            laterality = random.choice(['left', 'right'])  # nosec B311
+            breast_birads = random.choice(birads_level)  # nosec B311
+            breast_density = random.choice(density_level)  # nosec B311
+
+            for g_id, side_view in enumerate(['cc', 'mlo']):
+                # Save to PNG format
+
+                os.makedirs(os.path.join(str(root_dir), 'images', study_id), exist_ok=True)
+                png_file_path = os.path.join(str(root_dir), 'images', study_id, str(g_id)+'.png')
+                image = np.random.randint(0, 256, size=(384, 384))
+                cv2.imwrite(png_file_path, image)  # lossless
+
+                # Build metadata
+                metadata = fiftyone.ImageMetadata.build_for(png_file_path)
+                sample = fiftyone.Sample(
+                    filepath=png_file_path,
+                    metadata=metadata,
+                )
+
+                tags = []
+                detections = []
+
+                view_position = side_view
+                tags += [split, laterality, side_view]
+
+                # Remove duplicates and add to sample tags
+                tags = set(tags)
+                for tag in tags:
+                    sample.tags.append(tag)
+
+                # Grouped sample
+                sample['view_side'] = group.element(f'{laterality}_{view_position}')
+                sample['laterality'] = laterality
+                sample['view_position'] = view_position
+                sample['breast_birads'] = fiftyone.Classification(label=breast_birads)
+                sample['breast_density'] = fiftyone.Classification(label=breast_density)
+                sample['findings_objects'] = fiftyone.Detections(detections=detections)
+                sample['study_id'] = study_id
+
+                samples.append(sample)
+
+            dataset.add_samples(samples)
 
     output_dir = os.path.join(str(root_dir), 'temp_group_dataset')
     dataset.export(

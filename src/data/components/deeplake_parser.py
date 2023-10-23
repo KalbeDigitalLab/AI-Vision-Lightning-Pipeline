8c3b3fbbcdbdcf47fd6fd74c5779a58e79eb7b11
IstiqomahUswatunH <hasanah.10105@mail.ugm.ac.id>
2023-10-23 13:42:35 +0700

    update docstring

--- src/data/components/deeplake_parser.py
+++ src/data/components/deeplake_parser.py
@@ -8,55 +8,34 @@ from torch.utils.data import Dataset
 
 
 class DeepLakeDataset(Dataset):
-    """DeepLake Dataset.
+    """DeepLakeDataset. A PyTorch Dataset Wrapper for DeepLake Datasets.
 
-    This class is a PyTorch Dataset wrapper for DeepLake datasets, allowing you
-    to easily integrate them into your PyTorch-based machine learning pipelines.
+    This class serves as a PyTorch Dataset wrapper for DeepLake datasets. It allows you to seamlessly integrate DeepLake datasets into your PyTorch-based machine learning pipelines.
 
-    Parameters
-    ----------
-    data_dir : str or deeplake.Dataset
-        The DeepLake dataset object or the path to the dataset directory.
-        If `data_dir` is a string, it should be the path to the DeepLake dataset directory.
-        If `data_dir` is a deeplake.Dataset object, it represents the dataset directly.
-    transform : Optional[torchvision.transforms.Compose], optional
-        A data augmentation pipeline to apply to the images, by default None.
+    Parameters:
+        data_dir (str or deeplake.Dataset): The DeepLake dataset object or the path to the dataset directory. If `data_dir` is a string, it should be the path to the DeepLake dataset directory. If `data_dir` is a deeplake.Dataset object, it represents the dataset directly.
+        transform (Optional[torchvision.transforms.Compose], optional): A data augmentation pipeline to apply to the images. Defaults to None.
 
-    Raises
-    ------
-    TypeError
-        If the `data_dir` parameter is neither a valid DeepLake dataset object nor a string.
+    Raises:
+        TypeError: If the `data_dir` parameter is neither a valid DeepLake dataset object nor a string.
 
-    Attributes
-    ----------
-    transform : Optional[torchvision.transforms.Compose]
-        A data augmentation pipeline for transforming the images.
+    Attributes:
+        transform (Optional[torchvision.transforms.Compose]): A data augmentation pipeline for transforming the images.
 
-    Methods
-    -------
-    __len__()
-        Get the number of samples in the dataset.
-
-    __getitem__(idx)
-        Get a sample for the given index.
+    Methods:
+        __len__(): Get the number of samples in the dataset.
+        __getitem__(idx): Get a sample for the given index.
     """
 
     def __init__(self, data_dir, transform: Optional[torchvision.transforms.Compose] = None):
         """Initialize a DeepLakeDataset object.
 
-        Parameters
-        ----------
-        data_dir : str or deeplake.Dataset
-            The DeepLake dataset object or the path to the dataset directory.
-            If `data_dir` is a string, it should be the path to the DeepLake dataset directory.
-            If `data_dir` is a deeplake.Dataset object, it represents the dataset directly.
-        transform : Optional[torchvision.transforms.Compose], optional
-            A data augmentation pipeline to apply to the images, by default None.
+        Parameters:
+            data_dir (str or deeplake.Dataset): The DeepLake dataset object or the path to the dataset directory. If `data_dir` is a string, it should be the path to the DeepLake dataset directory. If `data_dir` is a deeplake.Dataset object, it represents the dataset directly.
+            transform (Optional[torchvision.transforms.Compose], optional): A data augmentation pipeline to apply to the images. Defaults to None.
 
-        Raises
-        ------
-        TypeError
-            If the `data_dir` parameter is neither a valid DeepLake dataset object nor a string.
+        Raises:
+            TypeError: If the `data_dir` parameter is neither a valid DeepLake dataset object nor a string.
         """
         self.transform = transform
         if isinstance(data_dir, str):
@@ -69,27 +48,20 @@ class DeepLakeDataset(Dataset):
     def __len__(self) -> int:
         """Get the number of samples in the dataset.
 
-        Returns
-        -------
-        int
-            The number of samples in the dataset.
+        Returns:
+            int: The number of samples in the dataset.
         """
         return len(self.data_dir)
 
     def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
         """Get a sample for the given index.
 
-        Parameters
-        ----------
-        idx : int
-            The index of the sample to retrieve.
+        Parameters:
+            idx (int): The index of the sample to retrieve.
 
-        Returns
-        -------
-        Tuple[torch.Tensor, torch.Tensor]
-            A tuple containing the image and its corresponding label as PyTorch tensors.
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: A tuple containing the image and its corresponding label as PyTorch tensors.
         """
-
         image = self.data_dir.images[idx].numpy()    # Convert the deeplake tensor to numpy array
         label = self.data_dir.labels[idx].numpy(fetch_chunks=True).astype(np.int32)
 

5007af59620d1bb79cc417ea8cb3a5e26c4e6bf5
IstiqomahUswatunH <hasanah.10105@mail.ugm.ac.id>
2023-10-23 11:14:57 +0700

    adjusting to changes

--- tests/test_bcc_datamodule.py
+++ tests/test_bcc_datamodule.py
@@ -1,32 +1,33 @@
-import pytest 
-import torch 
+import torch
 
-from src.data.bcc_datamodule import DeepLakeLitDataModule
-from tests.bc_dummy_dataset import (dummy_dataset,
-                                    dummy_train_dataset, 
-                                    dummy_val_dataset, 
-                                    dummy_test_dataset
-                                      )
+from src.data.bcc_datamodule import BreastCancerDataModule
+from tests.bc_dummy_dataset import (
+    deeplake_dummy_dataset_dir,
+    deeplake_dummy_test_dataset_dir,
+    deeplake_dummy_train_dataset_dir,
+    deeplake_dummy_val_dataset_dir,
+)
 
-def test_bc_classification_deeplake_lit_datamodule(dummy_train_dataset, dummy_val_dataset, dummy_test_dataset):
-    dm = DeepLakeLitDataModule(
-        train_dir=dummy_train_dataset,
-        val_dir=dummy_val_dataset,
-        test_dir=dummy_test_dataset
+
+def test_bc_classification_deeplake_lit_datamodule(deeplake_dummy_train_dataset_dir, deeplake_dummy_val_dataset_dir, deeplake_dummy_test_dataset_dir):
+    dm = BreastCancerDataModule(
+        train_dir=deeplake_dummy_train_dataset_dir,
+        val_dir=deeplake_dummy_val_dataset_dir,
+        test_dir=deeplake_dummy_test_dataset_dir
     )
     assert not dm.data_train and not dm.data_val and not dm.data_test
-    
+
     dm.setup()
-    
-    assert dm.data_train  and dm.data_val and dm.data_test
-    
+
+    assert dm.data_train and dm.data_val and dm.data_test
+
     train_dataloader = dm.train_dataloader()
     val_dataloader = dm.val_dataloader()
     test_dataloader = dm.test_dataloader()
 
     assert train_dataloader and val_dataloader and test_dataloader
-    assert len(dm.data_train) + len(dm.data_val) + len(dm.data_test)== 935
-    
+    assert len(dm.data_train) + len(dm.data_val) + len(dm.data_test) == 935
+
     batch = next(iter(train_dataloader))
     x, y = batch
     assert len(x) == 64

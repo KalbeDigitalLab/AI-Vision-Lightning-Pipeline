7e1eb3aa1aa34aa53011cd2e785cd436f68ba4c8
IstiqomahUswatunH <hasanah.10105@mail.ugm.ac.id>
2023-11-01 16:44:37 +0700

    add docstring

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,12 +1,17 @@
 import pytest
 import torch
+
 from src.models.components.layers.basic import ResNet18
+
 
 @pytest.fixture
 def ResNet18_fixture():
-    return ResNet18(3,3) 
+    """Fixture for creating a ResNet-18 model instance."""
+    return ResNet18(3, 3)
+
 
 def test_basic_layer(ResNet18_fixture):
+    """Test the basic layer of the ResNet-18 model."""
     model = ResNet18_fixture
 
     input = torch.rand(1, 3, 256, 256)
